4f342ee052a4446bf3ab96bd44d28189595dd8f0
thevasugupta <thevasugupta04@gmail.com>
2025-09-18 11:06:59 +0530

    PutMapping(/update)

--- SpringBootApplicationController.java
+++ SpringBootApplicationController.java
@@ -1,13 +1,13 @@
 package com.example.sb1;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +32,13 @@ public class SpringBootApplicationController {
 	@PostMapping("/addStudent")
 	public boolean addStudent(@RequestBody Student student) {
 		studentMap.put(student.getId(), student);
+		return true;
+	}
+	
+	@PutMapping("/update/{id}")
+	public boolean getStudents(@PathVariable String id, @RequestBody Student student) {
+		
+		studentMap.put(id, student);
 		return true;
 	}
 
